230437b0b07d2ea3bd3276a0519744117e154bcb
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-07-01 12:23:22 +0300

    Delete and Patch route

--- server/app.py
+++ server/app.py
@@ -18,7 +18,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -37,18 +36,37 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
+        plant = Plant.query.get_or_404(id)
+        return make_response(jsonify(plant.to_dict()), 200)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+
+    db.session.commit()
+    return jsonify(plant.to_dict()), 200
+
+
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get_or_404(id)
+
+    db.session.delete(plant)
+    db.session.commit()
+
+    return '', 204
 
 
 if __name__ == '__main__':
